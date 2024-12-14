99497c367e89e357537b3640fd9c95d18c3e3a3b
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-12-14 12:58:38 +0530

    Added floating navbar, projects section and integrated sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c70b180d610c30e52dc0b329f06692da@o4508465145249792.ingest.us.sentry.io/4508465147674624",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
